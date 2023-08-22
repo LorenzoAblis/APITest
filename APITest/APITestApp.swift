41758c25079daaffedc5ac1a864bbec15a07cb19
Lorenzo Ablis <lablis228609@gmail.com>
2023-08-22 11:05:30 -0500

    Initial Commit

---
+++ APITest/APITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  APITestApp.swift
+//  APITest
+//
+//  Created by Lorenzo J. Ablis on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct APITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APITest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  APITest
+//
+//  Created by Lorenzo J. Ablis on 8/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("apple")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
